acc62e0653088ab3eec25db655150b72d8bfc5b0
LiyaKuz <kuznetsova.smartsoft@gmail.com>
2022-05-23 15:34:59 +0300

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double rsl = -1;
-        return rsl;
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
