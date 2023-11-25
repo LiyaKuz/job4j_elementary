6554984307462076670664a3b3ab28bea72da302
LiyaKuz <kuznetsova.smartsoft@gmail.com>
2023-11-25 18:03:50 +0300

    Delete comments

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,15 +1,15 @@
 package ru.job4j.calculator;
 
-public class Calculator { //class
+public class Calculator {
 
-    public static void plus(int first, int second) { //function + (function arguments)
-        int result = first + second; //function body
-        System.out.println(result); //function body
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 
-    public static void minus(int first, int second) { //function + (function arguments)
-        int result = first - second; //function body
-        System.out.println(result); //function body
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
