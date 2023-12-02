20595930449dd9bb2181145b5c22f67e0de75b2f
LiyaKuz <kuznetsova.smartsoft@gmail.com>
2023-12-02 22:22:49 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -11,5 +11,14 @@ public class ArrayDefinition {
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
 
+        String[] names = new String[4];
+        names[0] = "Svetlana Viktorova";
+        names[1] = "Evgeniy Onegin";
+        names[2] = "Alexandra Istomina";
+        names[3] = "Oleg Svezhitskiy";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
